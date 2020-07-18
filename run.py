7870b1183cfd2f18231b760a2d4a75cc93707980
vaida898 <freelpfm@gmail.com>
2020-07-18 19:11:34 +0000

    added two blank lines between function and if statement in run.py file

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
